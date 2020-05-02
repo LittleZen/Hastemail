0c8eeddf559375673ed280794faf25dffae9a190
Zenek <35767830+Zenek-Hastro@users.noreply.github.com>
2020-05-02 10:51:15 +0200

    added function for check the entire blacklist!

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const bodyParser = require('body-parser');
 const sha256 = require("js-sha256");
+const editJsonFile = require("edit-json-file");
 //const router = express.Router();
 var fs = require('fs');
 
@@ -38,7 +39,7 @@ var Http = new XMLHttpRequest();
 //check json file 
 function testing() 
 {
-    let rawdata = fs.readFileSync('test.json');
+    let rawdata = fs.readFileSync('pvt.json');
     let mydata = JSON.parse(rawdata);
     console.log(mydata);
     return(mydata);
@@ -48,22 +49,21 @@ function testing()
 //filter json file
 function filter(email) 
 {
-    let rawdata = fs.readFileSync('pvt.json');
-    let mydata = JSON.parse(rawdata);
+  let rawdata = fs.readFileSync('pvt.json');
+  let mydata = JSON.parse(rawdata);
 
-     //
-    //const result = myData.filter((x) => x.email === email);
-   // 
-        const result = mydata.filter(obj => {
-          for (let [key, value] of Object.entries(obj)) {
-            if (value === email) {
-              return true;
-            }
-          }
-        });
-        console.log(result); // [ { '2': 'bad@gmail.com' } ]
-        return result.length > 0;
-      
+  const result = mydata.filter(obj => 
+  {
+    for (let [key, value] of Object.entries(obj)) 
+    {
+      if (value === email) 
+      {
+        return true;
+      }
+    }
+  });
+  console.log(result); // [ { '2': 'bad@gmail.com' } ]
+  return result.length > 0;   
 };
 
 
@@ -78,21 +78,57 @@ app.listen(PORT, function()
     console.log(`\nQui il server gira in ascolto, in attesa di ricevere la mail.\n\n Il server è in ascolto alla porta: ${PORT}`);
 });
 
-app.get('/json', function(req, res)
-{
-    var risultato = testing();
-    res.status(200).send(risultato).end();
-});
 
+//Check Server status
 app.get('/status', function(req, res)
 {
     res.status(200).send("Tutto ok!").end();
 });
 
-app.get('/pvt', function(req, res)
+
+//Check entire database 
+app.get('/list', function(req,res)
 {
-let example = filter("bad@gmail.com");
-console.log("risposta: " + example); // example è una variabile true/false, la puoi utilizzare per verificare se un email è presente!!
+  let lista = testing();
+  res.send(lista);
+});
+
+
+//Check if and id is present in to the list
+app.get('/check/:id', function(req, res)
+{
+  let user = req.params.id; // catch email address
+  let example = filter(user);
+  console.log(example); // example è una variabile true/false, la puoi utilizzare per verificare se una mail è presente!!
+
+  if(!example)
+  {
+    res.status(404).json({block: false}); // se non è presente, ritorna 404 + json false
+  }
+  else
+  {
+    res.status(302).json({block: true}); // se presente, ritorna 302 + json true
+  }
+});
+
+//Check Server status
+app.patch('/add/:id', function(req, res)
+{
+  let user = req.params.id; // catch email address
+  console.log("\nSto aggiungendo l'email: " + user + "\n");
+
+  let file = editJsonFile(`pvt.json`); 
+  file.set([], {1:user});
+  file.save();
+
+  file = editJsonFile(`pvt.json`, 
+  {
+    autosave: true
+  });
+
+  console.log(file.data);
+  res.send("\n\nfile sent" +file.toObject());
+
 });
 
 
